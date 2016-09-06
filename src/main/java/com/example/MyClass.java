d40b86ffe4e840c003ef908165a9e289e98202ef
joros96 <joros96@gmail.com>
2016-09-06 13:18:41 -0500

    aboutMe

---
+++ src/main/java/com/example/MyClass.java
@@ -0,0 +1,19 @@
+package com.example;
+
+public class MyClass
+{
+    public static void main(String[] args)
+    {
+        String myName = "Jonathan Oros";
+        String major = "Mechanical Engineering";
+        double gpa = 3.75;
+        char grade = 'A';
+        int year = 2014;
+        System.out.println("Hello, my name is " + myName);
+        System.out.println("I am majoring in " + major);
+        System.out.println("My goal this semester is to get an " + gpa + " or higher GPA");
+        System.out.println("The grade I hope to get in this class is an " + grade);
+        System.out.println("I first started college at the University of Iowa in " + year);
+
+    }
+}
